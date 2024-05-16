cd8053024092cb95e0c4e202ddfe1fcc5c57afbe
LETomppa <124377513+LETomppa@users.noreply.github.com>
2024-05-16 17:51:54 +0300

    Initial Commit

--- Node_crash_course/index.js
+++ Node_crash_course/index.js
@@ -51,7 +51,7 @@ const server = http.createServer((req, res) => {
     // Read FIle
     fs.readFile(filePath, (err, content) => {
         if (err) {
-            if (err.code == 'ENOENT') {
+            if (err.code == 'ENONET') {
                 // Page not found
                 fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
                     res.writeHead(200, { 'Content-Type': 'text/html' });
